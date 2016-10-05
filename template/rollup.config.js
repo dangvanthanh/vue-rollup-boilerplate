8c5dc786ffefc984a189306cbb748302a7fee3a7
Dang Van Thanh <dangvanthanh@dangthanh.org>
2016-10-05 23:00:16 +0700

    Improve with livereload and serve rollup plugins

--- template/rollup.config.js
+++ template/rollup.config.js
@@ -5,27 +5,48 @@ import nodeResolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import nodeGlobals from 'rollup-plugin-node-globals'
 import uglify from 'rollup-plugin-uglify'
+import { minify } from 'uglify-js'
+import livereload from 'rollup-plugin-livereload'
+import serve from 'rollup-plugin-serve'
 
-export default {
+const plugins = [
+  alias({
+    vue: 'node_modules/vue/dist/vue.js'
+  }),
+  vue({
+    css: './public/assets/css/app.css'
+  }),
+  buble({
+    objectAssign: 'Object.assign'
+  }),
+  nodeResolve({
+    jsnext: true,
+    main: true,
+    browser: true
+  }),
+  commonjs(),
+  nodeGlobals()
+]
+
+const config = {
   entry: './src/app.js',
   dest: './public/assets/js/app.js',
-  plugins: [
-    alias({
-      vue: 'node_modules/vue/dist/vue.js'
-    }),
-    vue({
-      css: './public/assets/css/app.css'
-    }),
-    buble({
-      objectAssign: 'Object.assign'
-    }),
-    nodeResolve({
-      jsnext: true,
-      main: true,
-      browser: true
-    }),
-    commonjs(),
-    nodeGlobals(),
-    (process.env.NODE_ENV === 'production' && uglify())
-  ]
+  sourceMap: true,
+  plugins: plugins
 }
+
+if (process.env.NODE_ENV === 'production') {
+  config.sourceMap = false
+  config.plugins.push(uglify({}, minify))
+}
+
+if (process.env.NODE_ENV === 'development') {
+  config.plugins.push(livereload())
+  config.plugins.push(serve({
+    contentBase: './public/',
+    port: 8080,
+    open: true
+  }))
+}
+
+export default config
