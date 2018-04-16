f19e630193f4a9c3dd6d8430475c803d85f9b7fe
Dang Van Thanh <dangvanthanh@dangthanh.org>
2018-04-16 23:54:21 +0700

    Add prettier

--- template/src/main.js
+++ template/src/main.js
@@ -1,13 +1,13 @@
-import Vue from 'vue'
+import Vue from 'vue';
 {{#router}}
-import VueRouter from 'vue-router'
+import VueRouter from 'vue-router';
 {{/router}}
-import App from './App.vue'
+import App from './App.vue';
 
 {{#router}}
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 {{/router}}
 
 const app = new Vue({
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
