de23af1ddb71e33dff7801a431af95d348f66de6
Dang Van Thanh <dangvanthanh@dangthanh.org>
2019-12-23 21:25:03 +0700

    Update latest

--- template/rollup.config.js
+++ template/rollup.config.js
@@ -4,36 +4,28 @@ import replace from 'rollup-plugin-replace';
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
-import { uglify } from 'rollup-plugin-uglify';
+import { terser } from 'rollup-plugin-terser';
 
 const production = !process.env.ROLLUP_WATCH;
 
-const plugins = [
-  postcss({ extract: true }),
-  vue({ css: false }),
-  replace({
-    'process.env.NODE_ENV': JSON.stringify('production')
-  }),
-  resolve(),
-  babel({
-    exclude: 'node_modules/**'
-  }),
-  commonjs()
-];
-
-let config = {
+export default {
   input: 'src/main.js',
   output: {
     file: 'dist/assets/app.js',
     format: 'iife',
     sourcemap: true
   },
-  plugins: plugins
-};
-
-if (production) {
-  config.output.sourcemap = false;
-  config.plugins.push(uglify());
+  plugins: [
+    postcss({ extract: true }),
+    vue({ css: false }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    resolve(),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    commonjs(),
+    production && terser()
+  ]
 }
-
-export default config;
